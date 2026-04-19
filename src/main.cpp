4cc480a158dd7619a46c96b2ad8efe86e6164f27
acidhood <nazar.kondrashov616@gmail.com>
2026-04-19 23:56:48 +0200

    fix_bag

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
-
 #include "Timer.h"
+#include <QSharedMemory>
+#include <QMessageBox>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSharedMemory sharedMemory("acidhood999_TimerQT_UniqueKey");
+
+    if (sharedMemory.attach())
+    {
+        QMessageBox::warning(nullptr, "Warning", "The application is already launched!");
+        return 0;
+    }
+
+    if (!sharedMemory.create(1)) return 0;
+
     Timer w;
     w.show();
     return a.exec();
